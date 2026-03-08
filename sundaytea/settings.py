3b48d00a4146ab5433339779bd88dd5149843237
aravherman <aravherman04@gmail.com>
2026-03-08 15:35:27 +0530

    some ui changes

--- sundaytea/settings.py
+++ sundaytea/settings.py
@@ -76,7 +76,10 @@ WSGI_APPLICATION = 'sundaytea.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
