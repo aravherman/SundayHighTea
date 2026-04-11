2f2e02a94832d20abda05a2450b9c9f838a15f06
aravherman <aravherman04@gmail.com>
2026-04-11 23:59:21 +0530

    Update settings.py

--- sundaytea/settings.py
+++ sundaytea/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'sundaytea.wsgi.application'
 DATABASES = {
     "default": dj_database_url.config(
         default="sqlite:///db.sqlite3",
-        conn_max_age=600
+        conn_max_age=10000
     )
 }
 
